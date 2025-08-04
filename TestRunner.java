19f0d4bfd65d178485cf51a197ac24cce8e6c59a
SindhuInCode <142319051+SindhuInCode@users.noreply.github.com>
2025-08-04 19:45:51 +0530

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -1,12 +1,13 @@
+package runner;
+ 
 import org.junit.runner.RunWith;
+ 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+ 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-  features = "src/test/resources",
-  glue = "",
-  plugin = {"pretty"},
-  monochrome = true
-)
-public class TestRunner {}
+@CucumberOptions(features="featureFiles/loginDemo.feature",glue="Cucumber")
+public class runnerClass {
+ 
+}
+
